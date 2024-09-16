8b8ca437912e307fb413cae036a815ed34a1047a
IntegralTeam <repo@sfxdx.com>
2024-09-16 09:18:13 +0300

    Update testnet params

--- genesis/genesis_testnet.go
+++ genesis/genesis_testnet.go
@@ -19,26 +19,26 @@ var (
 	// TestnetParams are the params used for the testnet testnet
 	TestnetParams = Params{
 		TxFeeConfig: TxFeeConfig{
-			TxFee:                         50 * units.MilliDione,
-			CreateAssetTxFee:              100 * units.MilliDione,
-			CreateSubnetTxFee:             1 * units.Dione,
-			TransformSubnetTxFee:          10 * units.Dione,
-			CreateBlockchainTxFee:         10 * units.Dione,
+			TxFee:                         5 * units.Dione,
+			CreateAssetTxFee:              10 * units.Dione,
+			CreateSubnetTxFee:             300 * units.Dione,
+			TransformSubnetTxFee:          5 * units.Dione,
+			CreateBlockchainTxFee:         300 * units.Dione,
 			AddPrimaryNetworkValidatorFee: 0,
 			AddPrimaryNetworkDelegatorFee: 0,
-			AddSubnetValidatorFee:         units.MilliDione,
-			AddSubnetDelegatorFee:         units.MilliDione,
+			AddSubnetValidatorFee:         5 * units.Dione,
+			AddSubnetDelegatorFee:         5 * units.Dione,
 		},
 		StakingConfig: StakingConfig{
 			UptimeRequirement:         .8, // 80%
-			MinValidatorStake:         1 * units.Dione,
-			MaxValidatorStake:         3 * units.MegaDione,
-			MinDelegatorStake:         1 * units.Dione,
+			MinValidatorStake:         500 * units.KiloDione,
+			MinDelegatorStake:         500 * units.Dione,
+			MaxValidatorStake:         60 * units.MegaDione,
 			MinDelegationFee:          20000, // 2%
-			MinValidatorStakeDuration: 24 * time.Hour,
-			MaxValidatorStakeDuration: 365 * 24 * time.Hour,
-			MinDelegatorStakeDuration: 24 * time.Hour,
-			MaxDelegatorStakeDuration: 365 * 24 * time.Hour,
+			MinValidatorStakeDuration: 365 * 24 * time.Hour,
+			MaxValidatorStakeDuration: 6 * 365 * 24 * time.Hour,
+			MinDelegatorStakeDuration: 30 * 24 * time.Hour,
+			MaxDelegatorStakeDuration: 6 * 365 * 24 * time.Hour,
 			RewardConfig: reward.Config{
 				MaxConsumptionRate: .12 * reward.PercentDenominator,
 				MinConsumptionRate: .10 * reward.PercentDenominator,
