c3cc00ce99fe2b5020105095267ca760e3254212
IntegralTeam <repo@sfxdx.com>
2024-09-20 16:30:31 +0300

    Fix systemd script

--- scripts/run_blockchain_server_single_node.sh
+++ scripts/run_blockchain_server_single_node.sh
@@ -37,10 +37,11 @@ fi
 run_node() {
 echo -ne "[Unit]\nDescription=Service for odyssey network's validator\nAfter=network.target\nAfter=syslog.target\n\n[Service]\nType=simple\nUser=root\n
 ExecStart='$odysseygo_path' --public-ip=${BOOTSTRAP_PUBLIC_IPS[$1]} --http-host=${BOOTSTRAP_PUBLIC_IPS[$1]} --http-port=${BOOTSTRAP_HTTP_PORTS[$1]} --staking-port=${BOOTSTRAP_STAKING_PORTS[$1]} --db-dir=${BOOTSTRAP_DB_DIRS[$1]}/${BOOTSTRAP_NODE_NAMES[$1]} --log-dir=${BOOTSTRAP_DB_DIRS[$1]}/${BOOTSTRAP_NODE_NAMES[$1]}/logs --chain-config-dir="$ODYSSEY_PATH"/scripts/configs/validators --network-id=${BOOTSTRAP_NETWORK_IDS[$1]} --http-allowed-hosts=* --bootstrap-ips=$BOOTSTRAP_HOST_IPS --bootstrap-ids=$BOOTSTRAP_IDS --staking-tls-cert-file=${TLS_CERT_PATHS[$1]} --staking-tls-key-file=${TLS_KEY_PATHS[$1]}
-Restart=on-failure\nRestartSec=5\nPIDFile=/tmp/odyssey$i.pid\n\n[Install]\nWantedBy=default.target" > /etc/systemd/system/node-validator-$i.service
+Restart=on-failure\nRestartSec=5\nPIDFile=/tmp/odyssey$1.pid\n\n[Install]\nWantedBy=default.target" > /etc/systemd/system/node-validator-$1.service
+
 systemctl daemon-reload
-systemctl start node-validator-$i.service
-systemctl enable node-validator-$i.service
+systemctl start node-validator-$1.service
+systemctl enable node-validator-$1.service
 }
 
 run_node $1 &
