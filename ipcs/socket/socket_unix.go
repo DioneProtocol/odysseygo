8fb262fb4de3d47d3ecd0ec73a6f826d024a8d46
IntegralTeam <repo@sfxdx.com>
2025-01-17 14:38:09 +0300

    Fix nil pointer dereference

--- ipcs/socket/socket_unix.go
+++ ipcs/socket/socket_unix.go
@@ -48,7 +48,7 @@ func Dial(addr string) (*Client, error) {
 	c, err := net.DialUnix("unix", nil, unixAddr)
 	if err != nil {
 		if isTimeoutError(err) {
-			return nil, errReadTimeout{c.RemoteAddr()}
+			return nil, errReadTimeout{unixAddr}
 		}
 		return nil, err
 	}
